3f81999c0e545a46a0eb7c178664761f6e04ea61
usel34 <kadikesamanta8@gmail.com>
2025-02-03 08:49:08 +0200

    1111

--- app.py
+++ app.py
@@ -25,8 +25,22 @@ def about():
 def top_data();
     try:
         top_rezultati = get_top_results()
-        top_5 = sorted(top_rezultati, key=lambda x: (x['klikski'], x['laiks']))
+        top_5 = sorted(top_rezultati, key=lambda x: (x['klikski'], x['laiks'])){:5}
+        return jsonify(top_5), 200
+    except Exception:
+        return jsonify({'statuss': 'error'}), 500
 
+@app.route('/pievienot-rezultatu', methods=['POST'])
+def pievienot_rezultatu():
+    dati = request.json
+    try: 
+        pievienot(dati)
+        top_5 = sorted(get_top_results(), key=lambda x: (x['klikski'], x['laiks'])){:5}
+        with open('result.json', 'w', encoding='utf-8') as f:
+            json.dump(top_5, f, ensure_ascii=False, indent=4)
+        return jsonify({'statuss': 'success'}), 200
+    except Exception:
+        return jsonify({'statuss': 'error'}), 500
 
 
 if __name__ == '__main__':
